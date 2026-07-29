a7de68e7ea5fb492e41a832ab78bd2d5baab9744
applemagic-01 <thatuserpc@gmail.com>
2026-07-29 20:18:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-//your JS code here. If required.
 function daysOfAYear(year) {
+    if (year < 1 || year > 9999) {
+        return "Invalid year";
+    }
+
     if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
         return 366;
-    } else {
-        return 365;
     }
+
+    return 365;
 }
+
+let year = Number(prompt("Enter a year"));
+alert(daysOfAYear(year));
